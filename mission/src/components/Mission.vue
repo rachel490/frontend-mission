5b795335cc48ac90ba9c39c95b88c17f468f11b9
rachel490 <ccori12@naver.com>
2022-01-08 18:50:55 +0900

    Feat: input과 제출버튼 추가
    input에 입력한 문자열이 아래에 출력될 수 있도록 하였으며 제출버튼을 눌렀을 때 문자열을 왼쪽으로 한칸씩 이동시킴.

---
+++ mission/src/components/Mission.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-model="value" />
+    <button type="button" v-on:click="move">제출</button>
+    <button type="button">알림</button>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    move() {
+      const str = [...this.value];
+      str.push(str[0]);
+      str.shift();
+      this.value = str.join("");
+    },
+  },
+};
+</script>
+
+<style></style>
