72f0e5fa29af6dabbec5231e4daefd2134533c11
rachel490 <ccori12@naver.com>
2022-01-16 05:04:08 +0900

    Feat: add Font Awesome regular

--- mission/src/main.js
+++ mission/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
-library.add(fas, fab);
+library.add(fas, fab, far);
 
 createApp(App).component('fa', FontAwesomeIcon).use(store).use(router).mount('#app');
