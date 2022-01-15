dd64448afaa893fb906711ad3c7f4f4a87165865
rachel490 <ccori12@naver.com>
2022-01-16 08:41:51 +0900

    Style: add global style

--- mission/src/main.js
+++ mission/src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+import "./assets/global.css";
 
 library.add(fas, fab, far);
 
-createApp(App).component('fa', FontAwesomeIcon).use(store).use(router).mount('#app');
+createApp(App).component("fa", FontAwesomeIcon).use(store).use(router).mount("#app");
