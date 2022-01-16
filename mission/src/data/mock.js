bfa0166f788aefc1d5eff1cbb65aab35ac20d1bd
rachel490 <ccori12@naver.com>
2022-01-16 21:45:53 +0900

    Feat: add dummy data

---
+++ mission/src/data/mock.js
@@ -0,0 +1,38 @@
+const products =
+  {
+    images: [
+      {
+        main: "https://static.zara.net/photos///2022/V/0/1/p/5039/533/720/2/w/1126/5039533720_1_1_1.jpg?ts=1638883106587",
+      },
+    ],
+    market: {
+      name: "Zara",
+      tags: ["러블리", "페미닌"],
+      rate: 95,
+      likes: 1114,
+    },
+    info: {
+      name: "Soft Dress",
+      originalPrice: 30800,
+      likes: "2.8만",
+      onSale: true,
+      salePercentage: "4",
+      salePrice: 29800,
+    },
+    reviews: [
+      {
+        title: "만족해요",
+        name: "최**",
+        time: "2022.01.09",
+        likes: 537,
+        images: [
+          "http://ririgirl.co.kr/web/upload/NNEditor/20210602/7_shop1_121322.jpg",
+          "http://ririgirl.co.kr/web/upload/NNEditor/20210602/8_shop1_121322.jpg",
+          "http://ririgirl.co.kr/web/upload/NNEditor/20210602/9_shop1_121322.jpg",
+        ],
+        text: "믿고 구매했어요! 너무 좋아요! 아무곳에나 쉽게 매칭할 수 있는 옷인것 같아요. 키가 작으시다면 조금 클수 있을 것 같긴 하지만 160이상이면 괜찮을 것 같습니다.",
+      },
+    ],
+  };
+
+export default products;
