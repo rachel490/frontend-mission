5ee3a1efa2b761346065cd976c93547d397d7a6a
rachel490 <ccori12@naver.com>
2022-01-16 21:10:33 +0900

    Feat: render details component w/ styling

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -2,17 +2,21 @@
   <div id="item-info-page">
     <preview-image></preview-image>
     <main-info></main-info>
+    <div id="detailsSection"><detail-info></detail-info></div>
+    <Review></Review>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import DetailInfo from '../components/DetailInfo.vue';
 import Footer from '../components/Footer.vue';
 import MainInfo from '../components/MainInfo.vue';
 import PreviewImage from '../components/PreviewImage.vue';
+import Review from '../components/Review.vue'
 
 export default {
-  components: { PreviewImage, MainInfo, Footer},
+  components: { PreviewImage, MainInfo, Footer, Review, DetailInfo },
   name: 'ItemInfoPage',
   data() {
     return {};
@@ -25,9 +29,19 @@ export default {
 <style scoped>
 #item-info-page {
   width: 100%; 
-  height: 1200px;
+  height: auto;
   z-index:1;
   background-color: white;
+  padding-bottom: 100px;
+}
+
+#detailsSection {
+  background-color: white;
+  width: 100%;
+  padding: 10px;
+  border-bottom: 1px solid rgba(0,0,0,.1);
+  border-top: 1px solid rgba(0,0,0,.1);
+
 }
 
 </style>
