23657be3a89bf076975a250141311bc88328f29c
rachel490 <ccori12@naver.com>
2022-01-21 04:48:30 +0900

    Feat: add List Page Title

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="item-list-page"></div>
+  <div id="item-list-page">
+    <h1 data-test="title">{{title}}</h1>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ItemListPage',
+  data() {
+    return {
+      title: 'My Shopping Mall',
+    }
+  }
 };
 </script>
 
