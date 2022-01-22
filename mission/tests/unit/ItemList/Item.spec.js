074cdf7a2ca0484d5a8f71923d83156b900a2034
rachel490 <ccori12@naver.com>
2022-01-23 01:53:14 +0900

    Test: add TC for product image

--- mission/tests/unit/ItemList/Item.spec.js
+++ mission/tests/unit/ItemList/Item.spec.js
@@ -1,10 +1,28 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/components/ItemList/Item.vue';
+import Item from '@/components/ItemList/Item.vue';
 
-describe('ItemListItem', () => {
-  it('redners ItemListItem', () => {
-    const wrapper = mount(ItemInfoPage);
+describe('Item', () => {
+  it('renders Item', () => {
+    const wrapper = mount(Item);
 
     expect(wrapper.find('.item-list-item').exists()).toBe(true);
   });
+
+  //product 사진있는지 확인
+  it('renders product image', async() => {
+    const wrapper = mount(Item);
+
+    await wrapper.setData({
+      img : 'https://images.unsplash.com/photo-1601762603339-fd61e28b698a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    })
+
+  expect(wrapper.find('img').exists()).toBe(true);
+  expect(wrapper.find('img').attributes('src')).toEqual(wrapper.vm.img);
+  })
 });
+
+
+// product 이름있는지 확인
+// product 가격있는지 확인
+// product description있는지 확인
+// product 할인이있는 경우 할인율 조건부 렌더링되는지 확인
