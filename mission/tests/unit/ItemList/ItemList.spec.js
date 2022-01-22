265e3a213c894cb3deb244e247de8260234bc2de
rachel490 <ccori12@naver.com>
2022-01-22 23:45:32 +0900

    Test: fix spelling mistakes

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/views/ItemList.vue';
+import ItemListPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
+  it('renders ItemListPage', () => {
+    const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
   });
