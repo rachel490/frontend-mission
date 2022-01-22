efccf19ec3cd6cef959bef41fae6196a42c2b186
rachel490 <ccori12@naver.com>
2022-01-23 02:01:31 +0900

    Feat: add product name in Item

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -2,6 +2,7 @@
   <div class="item-list-item">
     <h1>product list</h1>
     <img data-test="img" v-bind:src="img" />
+    <h2 data-test="name">{{name}}</h2>
   </div>
 </template>
 
@@ -11,6 +12,7 @@ export default {
   data() {
     return {
       img: 'https://images.unsplash.com/photo-1607382007923-6ba0d39f8617?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=848&q=80',
+      name: 'basic cotton tshirt',
     };
   },
 };
