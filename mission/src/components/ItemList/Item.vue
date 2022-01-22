cf48209f8a55fce9c873eb405bd01111d90cf1ce
rachel490 <ccori12@naver.com>
2022-01-23 01:54:44 +0900

    Feat: add product image in Item

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="item-list-item"></div>
+  <div class="item-list-item">
+    <h1>product list</h1>
+    <img data-test="img" v-bind:src="img" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ItemListItem',
+  data() {
+    return {
+      img: 'https://images.unsplash.com/photo-1607382007923-6ba0d39f8617?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=848&q=80',
+    };
+  },
 };
 </script>
 
