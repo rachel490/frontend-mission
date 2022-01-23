91fdc1e7b0b2b3257e97cb28a9f9e4e349abc27b
rachel490 <ccori12@naver.com>
2022-01-23 20:41:30 +0900

    Refactor: change name and delete unused data

--- mission/src/assets/mock.js
+++ mission/src/assets/mock.js
@@ -1,42 +1,42 @@
 const products = [
-	{
-		img:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=840&q=80",
-		name:"training set",
-		desc:"yello training set. cropped top.",
-		onSale:true,
-		originalPrice: 98000,
-		discountedPrice: 56000,
-	},
-	{
-		img:"https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80",
-		name:"flower printed dress",
-		desc:"white flower printed dress. Has a slit in the center",
-		onSale:false,
-		originalPrice:59000,
-	},
-	{
-		img:"https://images.unsplash.com/photo-1475180098004-ca77a66827be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
-		name:"brown knit",
-		desc:"brown knit top with unique pattern. ",
-		onSale:true,
-		originalPrice:56000,
-		discountedPrice:34400,
-	},
-	{
-		img:"https://images.unsplash.com/photo-1566206091558-7f218b696731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
-		name:"white setup",
-		desc:"linen white setup. Top is not a jacket but sleeveless top",
-		onSale:true,
-		originalPrice:150000,
-		discountedPrice:135000,
-	},
-	{
-		img:"https://images.unsplash.com/photo-1509631102211-5965b3c98b07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80",
-		name:"striped pants",
-		desc:"striped blazer pants. color is darkish green.",
-		onSale:false,
-		originalPrice:73000,
-	}
-]
+  {
+    img: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=840&q=80',
+    name: 'training set',
+    desc: 'yello training set. cropped top.',
+    onSale: true,
+    originalPrice: 98000,
+    discountPrice: 56000,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80',
+    name: 'flower printed dress',
+    desc: 'white flower printed dress. Has a slit in the center',
+    onSale: false,
+    originalPrice: 59000,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1475180098004-ca77a66827be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80',
+    name: 'brown knit',
+    desc: 'brown knit top with unique pattern. ',
+    onSale: true,
+    originalPrice: 56000,
+    discountPrice: 34400,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1566206091558-7f218b696731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+    name: 'white setup',
+    desc: 'linen white setup. Top is not a jacket but sleeveless top',
+    onSale: true,
+    originalPrice: 150000,
+    discountPrice: 135000,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1509631102211-5965b3c98b07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
+    name: 'striped pants',
+    desc: 'striped blazer pants. color is darkish green.',
+    onSale: false,
+    originalPrice: 73000,
+  },
+];
 
 export default products;
